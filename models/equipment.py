0b3ced6669e0bb5992613e3db5105a469e44e137
Izaskun Ortiz (Algios) <IzaskunAlgios@users.noreply.github.com>
2015-09-14 13:37:19 +0200

    Revert "Menu SAT"

--- models/equipment.py
+++
@@ -1,9 +0,0 @@
-# b-*- encoding: utf-8 -*-
-
-from openerp import models, fields
-
-class Equipment(models.Model):
-    _name = 'sat.equipment'
-
-    name = fields.Char(string="Equipment Name", required=True)
-    description = fields.Text(string="Equipment Description")
