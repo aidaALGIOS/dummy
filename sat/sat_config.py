d944f801be87707739ff2761dce96e6d4190609a
javialgios <javi.melendez@algios.com>
2015-09-25 13:27:16 +0200

    Añadir nuevos campos a SAT config 	-Working time rounding 	-Invoicing time rounding 	-Register working time in time slots

--- sat/sat_config.py
+++ sat/sat_config.py
@@ -20,7 +20,8 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
 
 class SatConfiguration(models.TransientModel):
     _inherit = 'res.config.settings'
@@ -40,10 +41,51 @@ class SatConfiguration(models.TransientModel):
         help="This allows you to define special conditions to manage and invoice your SAT tasks."
     )
 
+    work_time_rounding = fields.Integer(
+        string="Working time rounding",
+        default=5,
+        help="Minimum working time unit (minutes). Every worktime will be rounded before saving. " \
+             "If you change it, every registered worktime lines will remain unchanged."
+    )
+
+    invoice_time_rounding = fields.Integer(
+        string="Invoicing time rounding",
+        default=15,
+        help="Minimum invoicing time unit (minutes). Every invoiceable duration will be rounded before invoicing. " \
+             "If you change it, every registered invoice lines will remain unchanged. You can change the value in any contracts."
+    )
+
+    register_work_time = fields.Boolean(
+        string="Register working time in time slots",
+        help="Check if you want to know exactly when every action begins and ends. Uncheck if you only want to know the duration."
+    )
+
+    @api.one
+    @api.constrains('work_time_rounding', 'invoice_time_rounding')
+    def _check_positive_rounding(self):
+        if self.work_time_rounding < 1:
+            raise ValidationError(_("Working time rounding must be greater than 0"))
+        if self.invoice_time_rounding < 1:
+            raise ValidationError(_("Invoicing time rounding must be greater than 0"))
+
+    @api.model
+    def get_default_company_values(self, fields):
+        """Recuperar la configuración de la compañía"""
+        company = self.env.user.company_id
+        return {
+            'work_time_rounding': company.work_time_rounding,
+            'invoice_time_rounding': company.invoice_time_rounding,
+            'register_work_time': company.register_work_time
+        }
+
     @api.one
     def set_company_values(self):
+        """Guardar la configuración en la compañía"""
         company = self.env.user.company_id
         company.write({
             'manage_equipments': self.group_manage_equipments,
-            'manage_contracts': self.group_manage_contracts
+            'manage_contracts': self.group_manage_contracts,
+            'work_time_rounding': self.work_time_rounding,
+            'invoice_time_rounding': self.invoice_time_rounding,
+            'register_work_time': self.register_work_time
         })
